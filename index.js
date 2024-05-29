ce2c0aff5fd55c5458725a9143fdaf8abda0dc02
codal-mchandarana <mchandarana@codal.com>
2024-05-29 11:44:27 +0530

    Commiting changes

--- index.js
+++ index.js
@@ -10,24 +10,28 @@ import {
 } from "@aws-sdk/lib-dynamodb";
 
 const app = express();
-const client = new DynamoDBClient({ region: "eu-north-1" });
-const docClient = DynamoDBDocumentClient.from(client);
+const client = new DynamoDBClient({region: "eu-north-1"});
+const docClient = DynamoDBDocumentClient.from(client, {
+    marshallOptions: {
+        removeUndefinedValues: true,
+    }
+});
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("Hello")
 })
 
-app.get('/connection',async (req,res)=>{
+app.get('/connection', async (req, res) => {
     const command = new ListTablesCommand({});
 
     const response = await docClient.send(command);
     return res.status(200).json(response)
 })
 
-app.get('/data',async (req,res)=>{
+app.get('/data', async (req, res) => {
     const command = new DescribeTableCommand({
         TableName: "product",
     });
@@ -36,37 +40,37 @@ app.get('/data',async (req,res)=>{
     return res.status(200).json(response)
 })
 
-app.get('/getDataById/:id',async (req,res)=>{
+app.get('/getDataById/:id', async (req, res) => {
 
     const id = parseInt(req.params.id);
     const command = new GetCommand({
         TableName: "product",
-        Key:{id:id}
+        Key: {id: id}
     });
 
     const response = await docClient.send(command);
     return res.status(200).json(response)
 })
 
-app.get('/getDataScan/:id',async (req,res)=>{
+app.get('/getDataScan/:id', async (req, res) => {
     const id = parseInt(req.params.id);
 
     const command = new ScanCommand({
         TableName: "product",
-        FilterExpression:"#id = :id",
+        FilterExpression: "#id = :id",
         ExpressionAttributeNames:{
             "#id" : "id"
         },
-        ExpressionAttributeValues:{
-            ":id": { "N" : id }
-        }
+        ExpressionAttributeValues: {
+            ":id": id
+        },
     });
 
     const response = await docClient.send(command);
     return res.status(200).json(response)
 })
 
-app.get('/getAllData',async (req,res)=>{
+app.get('/getAllData', async (req, res) => {
     const command = new ScanCommand({
         TableName: "product",
     });
@@ -75,27 +79,27 @@ app.get('/getAllData',async (req,res)=>{
     return res.status(200).json(response)
 })
 
-app.post('/addProduct',async(req,res)=>{
-    const {id,Brand,Description,Name} = req.body;
+app.post('/addProduct', async (req, res) => {
+    const {id, Brand, Description, Name} = req.body;
 
     const command = new PutCommand({
-        TableName:"product",
-        Item:{id,Brand,Description,Name}
+        TableName: "product",
+        Item: {id, Brand, Description, Name}
     })
 
     const response = await docClient.send(command);
     return res.status(200).json(response)
 })
 
-app.put('/editProduct',async(req,res)=>{
-    const {id,key,value} = req.body;
+app.put('/editProduct', async (req, res) => {
+    const {id, key, value} = req.body;
 
     const command = new UpdateCommand({
-        TableName:"product",
-        Key:{
-            id:id
+        TableName: "product",
+        Key: {
+            id: id
         },
-        UpdateExpression:`set #${key} = :value`,
+        UpdateExpression: `set #${key} = :value`,
         ExpressionAttributeNames: {
             [`#${key}`]: `${key}`
         },
@@ -107,12 +111,12 @@ app.put('/editProduct',async(req,res)=>{
     return res.status(200).json(response)
 })
 
-app.delete('/deleteProduct/:id',async(req,res)=>{
+app.delete('/deleteProduct/:id', async (req, res) => {
     const id = parseInt(req.params.id);
 
     const command = new DeleteCommand({
-        TableName:"product",
-        Key:{
+        TableName: "product",
+        Key: {
             id: id
         }
     });
@@ -121,6 +125,6 @@ app.delete('/deleteProduct/:id',async(req,res)=>{
     return res.status(200).json(response)
 })
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("PORT IS RUNNING ON PORT 3000")
 })
